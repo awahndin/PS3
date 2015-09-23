b7b7768e3058fa5af573c6a71637307695628447
Awah Ndingwan <Awahndin@udel.edu>
2015-09-22 20:33:18 -0400

    iuuhj

---
+++ src/Account.java
@@ -0,0 +1,11 @@
+//import calender?
+public class Account {
+	private int id = 0;
+	private double balance = 0;
+	private double annualIntrestRate = 0;
+//	private date dateCreated = ???
+	
+	public static void main(String[] args){
+	
+	}
+}
